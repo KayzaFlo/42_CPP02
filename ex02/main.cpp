71fafb3b731f9f0cd9bcecf391b2d3a004c8e8df
fgeslin <fgeslin@student.42quebec.com>
2023-04-25 12:14:59 -0400

    add tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fgeslin <fgeslin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 14:20:39 by fgeslin           #+#    #+#             */
-/*   Updated: 2023/04/24 13:28:14 by fgeslin          ###   ########.fr       */
+/*   Updated: 2023/04/25 12:14:37 by fgeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,24 @@
 int main( void ) {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "++a = " <<  ++a << std::endl;
+	std::cout << "a = " <<  a << std::endl;
+	std::cout << "a++ = " <<  a++ << std::endl;
+	std::cout << "a = " <<  a << std::endl;
+	std::cout << "b = " <<  b << std::endl;
+	std::cout << "b / 2 = " <<  b / 2 << std::endl;
+	a = a * 2;
+	std::cout << "a = a * 2 = " <<  a << std::endl;
+	std::cout << "a + b = " <<  a + b << std::endl;
+	std::cout << "b - a = " <<  b - a << std::endl;
+	std::cout << "max(a,b) = " <<  Fixed::max( a, b ) << std::endl;
+	std::cout << "min(a,b) = " <<  Fixed::min( a, b ) << std::endl;
+
+	std::cout << "a == a ? " <<  (a == a ? "true" : "false") << std::endl;
+	std::cout << "a == b ? " <<  (a == b ? "true" : "false") << std::endl;
+	std::cout << "a < b ? " <<  (a < b ? "true" : "false") << std::endl;
+	std::cout << "a > b ? " <<  (a > b ? "true" : "false") << std::endl;
+
 	return 0;
 }
